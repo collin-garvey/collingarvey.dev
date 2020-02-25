e65f8717d3e5d61de6148f9c3347fe69d5b51764
Collin Garvey <collin.garvey@gmail.com>
2020-02-24 21:10:26 -0500

    Layout updoots

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,5 @@
+const Layout = () => {
+  return <main></main>;
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import styles from './Home.module.css';
+
 const Home = () => {
   return (
     <>
-      <h1>Collin Garvey</h1>
-      <h2>Web Engineer + Manager</h2>
+      <div className={styles.intro}>
+        <h1>Collin Garvey</h1>
+        <h2>Web Engineer + Manager</h2>
+      </div>
     </>
   );
 };
