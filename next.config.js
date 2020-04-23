d2a51c07a6255e9b6b81ec13f4ec39b823951d52
Collin Garvey <collin.garvey@gmail.com>
2020-04-23 15:55:26 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   poweredByHeader: false,
   webpack: config => {
@@ -7,4 +10,8 @@ module.exports = {
     });
     return config;
   },
+  assetPrefix:
+    isDev || isProd
+      ? `https://${process.env.CG_WEB_S3_BUCKET_NAME}.s3.amazonaws.com`
+      : '',
 };
