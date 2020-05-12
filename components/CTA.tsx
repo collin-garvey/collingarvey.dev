fb09f720a7893c6015ab4cc308178345bd6cf980
Collin Garvey <collin.garvey@gmail.com>
2020-05-12 12:22:21 -0400

    Clean up and adjust some about page content

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './CTA.module.css';
 
-const CTA = props => {
+const CTA: React.SFC = props => {
   return <div className={styles.CTA}>{props.children}</div>;
 };
 
